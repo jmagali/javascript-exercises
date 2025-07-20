b73e43917b664fc4cabfcba1bfc6c2bb1f095839
JM <jmagali.dev@gmail.com>
2025-07-19 22:20:59 -0400

    Complete sumAll exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if ((a < 0 || b < 0) 
+        || !(Number.isInteger(a) && Number.isInteger(b)) 
+        || (typeof a !== "number" && typeof b !== "number")) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    if (a > b) {
+        max = a;
+        min = b;
+    }
+    else {
+        max = b;
+        min = a;
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
