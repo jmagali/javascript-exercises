a6f31250c11dac4ff3c8c8f680b1e120498f5fdb
JM <jmagali.dev@gmail.com>
2025-07-22 23:44:50 -0400

    Complete find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.sort( (a, b) => {
 
-};
+        const currentDate = new Date();
+
+        let aAge = (a.yearOfDeath ?? currentDate.getFullYear()) - a.yearOfBirth;
+        let bAge = (b.yearOfDeath ?? currentDate.getFullYear()) - b.yearOfBirth;
+
+        return bAge - aAge;
+    })[0];
+};  
 
 // Do not edit below this line
 module.exports = findTheOldest;
