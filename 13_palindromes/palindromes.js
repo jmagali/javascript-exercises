22aff6d78caf0d142a7a0180e1e321214b6a6980
JM <jmagali.dev@gmail.com>
2025-07-22 23:17:44 -0400

    Complete palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const valid = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    const validStr = string
+    .toLowerCase()
+    .split("")
+    .filter(char => valid.includes(char))
+    .join("");
+
+    const reverseStr = validStr.split("").reverse().join("");
+
+    if (reverseStr === validStr) {
+        return true;
+    }
+
+    return false;
 
 };
 
